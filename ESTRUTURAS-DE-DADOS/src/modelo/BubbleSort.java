d3dfda3191f4d73d6c0e644f3da223e00eaff449
Hicaro_Brasil <hicaro.brasil@hotmail.com>
2022-10-29 14:45:14 -0300

    Update BubbleSort.java

--- ESTRUTURAS-DE-DADOS/src/modelo/BubbleSort.java
+++ ESTRUTURAS-DE-DADOS/src/modelo/BubbleSort.java
@@ -4,22 +4,20 @@ import utilitarios.TrocaElementos;
 
 public class BubbleSort {
         public static void bubbleSort(long[] vetor) {
-                int n = vetor.length;
-                boolean controle = true;
-                for (int i = 0; i < n; i++) {
-                        for (int j = 1; j < (n - i); j++) {
+                int tamanhoVetor = vetor.length;
+                for (int i = 0; i < tamanhoVetor; i++) {
+                        boolean trocou = false;
+                        for (int j = 1; j < (tamanhoVetor - i); j++) {
                                 if (vetor[j - 1] > vetor[j]) {
                                         // troca elementos
                                         TrocaElementos.trocar(vetor, j - 1, j);
-                                        controle = false;
+                                        trocou = true;
                                 }
-
                         }
-                        //Aqui é feito a otimização
-                        if (controle) {
+                        //Aqui ï¿½ feito a otimizaï¿½ï¿½o
+                        if (!trocou) {
                                 break;
                         }
                 }
-
         }
 }
