40116a40da2ee928ce03e36248e1c15a5b3a6e9a
Hicaro_Brasil <hicaro.brasil@hotmail.com>
2022-11-04 22:32:00 -0300

    upando o resto do Relatorio1

--- ESTRUTURAS-DE-DADOS/src/utilitarios/TrocaElementos.java
+++ ESTRUTURAS-DE-DADOS/src/utilitarios/TrocaElementos.java
@@ -1,9 +1,6 @@
 package utilitarios;
 
 public class TrocaElementos {
-	public static void trocar(String[] sequencia, int i, int j) {
-=======
->>>>>>> Stashed changes
 
 	public static void trocar(long[] v, int i, int j) {
 		long aux = v[i];
@@ -12,10 +9,6 @@ public class TrocaElementos {
 	}
 
 	public static void trocarString(String[] sequencia, int i, int j) {
-<<<<<<< Updated upstream
-=======
->>>>>>> 3f5ce9ad68d25b204fc21a7b5929e0c37f632b00
->>>>>>> Stashed changes
 		String aux = sequencia[i];
 		sequencia[i] = sequencia[j];
 		sequencia[j] = aux;
