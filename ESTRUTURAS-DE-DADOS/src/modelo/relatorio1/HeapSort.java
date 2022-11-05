5677f9b17d2d56cbbee3dc2758f031937e55dba9
Hicaro_Brasil <hicaro.brasil@hotmail.com>
2022-11-05 16:51:17 -0300

    Upando HeapSort

--- ESTRUTURAS-DE-DADOS/src/modelo/relatorio1/HeapSort.java
+++ ESTRUTURAS-DE-DADOS/src/modelo/relatorio1/HeapSort.java
@@ -1,5 +1,19 @@
 package modelo.relatorio1;
 
-public class HeapSort {
+import utilitarios.TrocaElementos;
 
+public class HeapSort {
+    public static void heapSortLong(long[] vetor) {
+        int tamanho = vetor.length-1;
+        for(int i= 0;i< tamanho;i++){
+            int f = i + 1;
+            while(f > 1 && vetor[f/2] < vetor[f]){
+                TrocaElementos.trocar(vetor, f, f/2);
+                f /= 2;
+            }
+        }
+    }
+    public static void HeapSortString(String[] vetor) {
+
+    }
 }
--- ESTRUTURAS-DE-DADOS/src/modelo/relatorio1/SelecaoDireta.java
+++ ESTRUTURAS-DE-DADOS/src/modelo/relatorio1/SelecaoDireta.java
@@ -5,7 +5,6 @@ import utilitarios.TrocaElementos;
 public class SelecaoDireta {
 
     public static void selecaoDireta(long[] vetor) {
-
         for (int j = 0; j < vetor.length; j++) {
             int menor = j;
             for (int i = j + 1; i < vetor.length; i++) {
@@ -18,7 +17,6 @@ public class SelecaoDireta {
     }
 
     public static void selecaoDiretaString(String[] vetor) {
-
         for (int j = 1; j < vetor.length; j++) {
             int menor = j;
             for (int i = j + 1; i < vetor.length - 1; i++) {
