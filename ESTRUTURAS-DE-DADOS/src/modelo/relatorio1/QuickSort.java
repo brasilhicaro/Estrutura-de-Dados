777ca6197e0b602c0dd89f5cb4f2c85311c67987
Augusto Almeida <94397552+AugustoFerreiraAlmeida@users.noreply.github.com>
2022-11-06 00:18:34 -0300

    Update QuickSort.java

--- ESTRUTURAS-DE-DADOS/src/modelo/relatorio1/QuickSort.java
+++ ESTRUTURAS-DE-DADOS/src/modelo/relatorio1/QuickSort.java
@@ -6,49 +6,23 @@ import utilitarios.TrocaElementos;
 
 public class QuickSort {
 
-	public static void quickSortOrdenar(String[] vetor, int inicio, int fim) {
-		if (fim > inicio) {
-			int indexPivo = inicio;// primeiro elemento como pivô.
-			
-			int esq = inicio;
-			int dir = fim;
-			
-			while(esq <= dir) {	
-				while(esq < fim && vetor[esq].compareTo(vetor[indexPivo]) <= 0) {	
-					esq++;	
-				}while(dir > inicio && vetor[dir].compareTo(vetor[indexPivo]) >= 0) {	
-					dir--;
-				}
-				if ( esq < dir) {
-					TrocaElementos.trocarString(vetor, esq, dir);
-					esq++;
-					dir--;
-				}	
-			}		
-			if(inicio < dir) {
-				quickSortOrdenar(vetor, inicio, dir);
-			}
-			if(esq < fim) {
-				quickSortOrdenar(vetor, esq, fim);
-			}		
-		}
-	}
-	
 	public static void quickSortOrdenarPivoAleatorio(String[] vetor, int inicio, int fim) {
-		
-		if (fim > inicio) {
-			int indexPivo = inicio + ((int) Math.random()*fim);      // Elemento aleatório como pivô.
+			Random random = new Random();
+			int indexPivo = random.nextInt(fim - inicio + 1) + inicio;      // Elemento aleatório como pivô.
+			String pivo = vetor[indexPivo];
 			int esq = inicio;
 			int dir = fim;
 			
 			while(esq <= dir) {	
-				while(esq < fim && vetor[esq].compareTo(vetor[indexPivo]) <= 0) {	
+				while(esq < fim && vetor[esq].compareTo(pivo) < 0) {	
 					esq++;	
-				}while(dir > inicio && vetor[dir].compareTo(vetor[indexPivo]) >= 0) {	
+				}while(dir > inicio && vetor[dir].compareTo(pivo) > 0) {	
 					dir--;
 				}
-				if ( esq < dir) {
-					TrocaElementos.trocarString(vetor, esq, dir);
+				if ( esq <= dir) {
+					String auxiliar = vetor[esq];
+					vetor[esq] = vetor[dir];
+					vetor[dir] = auxiliar;
 					esq++;
 					dir--;
 				}	
@@ -60,5 +34,30 @@ public class QuickSort {
 				quickSortOrdenarPivoAleatorio(vetor, esq, fim);
 			}		
 		}
+	
+	public static void quickSortPrimeiroElemento(String[] vetor, int inicio, int fim) {
+		String pivo = vetor[inicio]; 
+		int esquerda = inicio;
+		int direita = fim;
+		while (esquerda <= direita) {
+			
+			while (esquerda <= fim && vetor[esquerda].compareTo(pivo) < 0)
+				esquerda++;
+
+			while (direita >= inicio && vetor[direita].compareTo(pivo) > 0)
+				direita--;
+			
+			if (esquerda <= direita) {
+				String auxiliar = vetor[esquerda];
+				vetor[esquerda] = vetor[direita];
+				vetor[direita] = auxiliar;
+				esquerda++;
+				direita--;
+			}
+		}
+		if (esquerda < fim) 
+			quickSortPrimeiroElemento(vetor, esquerda, fim);		
+		if (direita > inicio) 
+			quickSortPrimeiroElemento(vetor, inicio, direita);
 	}
 }
