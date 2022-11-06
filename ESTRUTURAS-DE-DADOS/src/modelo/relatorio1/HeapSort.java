43155d13c4cd4caa3c74a859324b32fe1b4365d5
Augusto Almeida <94397552+AugustoFerreiraAlmeida@users.noreply.github.com>
2022-11-06 15:48:04 -0300

    Update HeapSort.java

--- ESTRUTURAS-DE-DADOS/src/modelo/relatorio1/HeapSort.java
+++ ESTRUTURAS-DE-DADOS/src/modelo/relatorio1/HeapSort.java
@@ -3,17 +3,41 @@ package modelo.relatorio1;
 import utilitarios.TrocaElementos;
 
 public class HeapSort {
-    public static void heapSortLong(long[] vetor) {
-        int tamanho = vetor.length-1;
-        for(int i= 0;i< tamanho;i++){
-            int f = i + 1;
-            while(f > 1 && vetor[f/2] < vetor[f]){
-                TrocaElementos.trocar(vetor, f, f/2);
-                f /= 2;
-            }
-        }
-    }
-    public static void HeapSortString(String[] vetor) {
-
-    }
+    
+	public static void construirHeap(String[] vetor, int i, int f) {
+		while(i <= f) {
+			int maior = i;
+			int esq = 2*i + 1;
+			int dir = 2*i + 2;
+			
+			if(esq <= f && vetor[esq].compareTo(vetor[maior]) > 0){
+				maior = esq;
+			}if(dir <= f && vetor[dir].compareTo(vetor[maior]) > 0){
+				maior = dir;
+			}
+			if(maior != i) {
+				String aux = vetor[maior];
+				vetor[maior] = vetor[i];
+				vetor[i] = aux;
+				i = maior;
+				
+			}else {
+				return;
+			}
+		}
+		
+	}public static void heap_sort(String[] vetor){
+		int f = vetor.length-1;
+		
+		for(int i = f; i >= 0; i--) {
+			construirHeap(vetor,i,f);
+		}
+		while(f > 0) {
+			String aux = vetor[f];
+			vetor[f] = vetor[0];
+			vetor[0] = aux;
+			construirHeap(vetor,0,--f);
+		}
+	}
+	
 }
