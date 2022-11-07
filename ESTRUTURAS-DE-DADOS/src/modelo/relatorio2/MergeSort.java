32cd5a7968f4af0f9084424d2323eea7e0641f92
Augusto Almeida <94397552+AugustoFerreiraAlmeida@users.noreply.github.com>
2022-11-07 09:33:33 -0300

    Update MergeSort.java

--- ESTRUTURAS-DE-DADOS/src/modelo/relatorio2/MergeSort.java
+++ ESTRUTURAS-DE-DADOS/src/modelo/relatorio2/MergeSort.java
@@ -1,5 +1,48 @@
 package modelo.relatorio2;
 
+import java.util.Arrays;
+
 public class MergeSort {
 
+	public static void intercala(long[] sequencia, int inicio, int meio, int fim) {
+		long novoVetor[] = new long[fim - inicio];
+		int i = inicio;
+		int m = meio;
+		int pos = 0;
+		while (i < meio && m < fim) {
+			if (sequencia[i] <= sequencia[m]) {
+				novoVetor[pos] = sequencia[i];
+				pos = pos + 1;
+				i = i + 1;
+			} else {
+				novoVetor[pos] = sequencia[m];
+				pos = pos + 1;
+				m = m + 1;
+			}
+		}
+		while (i < meio) {
+			novoVetor[pos] = sequencia[i];
+			pos = pos + 1;
+			i = i + 1;
+		}
+		while (m < fim) {
+			novoVetor[pos] = sequencia[m];
+			pos = pos + 1;
+			m = m + 1;
+		}
+		for (pos = 0, i = inicio; i < fim; i++, pos++) {
+			sequencia[i] = novoVetor[pos];
+		}
+	}
+
+	public static void mergeSortRecursivo(int inicio, int fim, long[] sequencia) {
+		if (inicio < fim - 1) {
+			int meio = (inicio + fim) / 2;
+			mergeSortRecursivo(inicio, meio, sequencia);
+			mergeSortRecursivo(meio, fim, sequencia);
+			intercala(sequencia, inicio, meio, fim);
+		}
+	}
 }
+
+//Fonte: http://www.universidadejava.com.br/pesquisa_ordenacao/merge-sort/
