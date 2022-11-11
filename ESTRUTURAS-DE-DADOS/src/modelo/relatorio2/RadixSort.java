e19ce27609b5ec851b57a1267ce674dccb8d9e2d
Augusto Almeida <94397552+AugustoFerreiraAlmeida@users.noreply.github.com>
2022-11-11 00:03:05 -0300

    Update RadixSort.java

--- ESTRUTURAS-DE-DADOS/src/modelo/relatorio2/RadixSort.java
+++ ESTRUTURAS-DE-DADOS/src/modelo/relatorio2/RadixSort.java
@@ -1,5 +1,47 @@
 package modelo.relatorio2;
 
-public class RadixSort {
+import java.util.List;
 
+public class RadixSort {
+	
+	public static void radixSort(int[] vetor) {
+		List[] auxiliar = new List[10];
+		
+		int maior = encontrarMaior(vetor);
+		
+		int qtd_repeticoes = 
+	}
+	
+	
+	public static int encontrarMaior(int[] vetor) {
+		
+		int maior = 0;
+		
+		for(int num :vetor) {
+			
+			if(num > maior) {
+				
+				maior = num;
+				
+			}
+			
+		}
+			
+		return maior;
+	}
+	
+	public static int encontrarNumDigitos(int num) {
+		
+		int contador = 0;
+		int numb;
+		while (num > 1 ) {
+			
+			num/10;
+			
+			
+		}
+		
+		return num;
+	}
+	
 }
