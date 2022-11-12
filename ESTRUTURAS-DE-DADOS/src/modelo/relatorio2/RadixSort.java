30a860e16457a82b21cd1633b59c1a4cf4f5a9e6
Augusto Almeida <94397552+AugustoFerreiraAlmeida@users.noreply.github.com>
2022-11-11 22:42:40 -0300

    Update RadixSort.java

--- ESTRUTURAS-DE-DADOS/src/modelo/relatorio2/RadixSort.java
+++ ESTRUTURAS-DE-DADOS/src/modelo/relatorio2/RadixSort.java
@@ -1,47 +1,62 @@
 package modelo.relatorio2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class RadixSort {
-	
-	public static void radixSort(int[] vetor) {
-		List[] auxiliar = new List[10];
-		
-		int maior = encontrarMaior(vetor);
-		
-		int qtd_repeticoes = 
-	}
-	
-	
-	public static int encontrarMaior(int[] vetor) {
-		
-		int maior = 0;
-		
-		for(int num :vetor) {
-			
-			if(num > maior) {
-				
-				maior = num;
-				
-			}
-			
-		}
-			
-		return maior;
-	}
-	
-	public static int encontrarNumDigitos(int num) {
-		
-		int contador = 0;
-		int numb;
-		while (num > 1 ) {
-			
-			num/10;
-			
-			
-		}
-		
-		return num;
-	}
-	
+
+	static int getMax(long[] sequencia, int n)
+    {
+        int mx = (int) sequencia[0];
+        for (int i = 1; i < n; i++)
+            if (sequencia[i] > mx)
+                mx = (int) sequencia[i];
+        return mx;
+    }
+ 
+    // A function to do counting sort of arr[] according to
+    // the digit represented by exp.
+    static void countSort(long[] sequencia, int n, int exp)
+    {
+        int output[] = new int[n]; // output array
+        int i;
+        int count[] = new int[10];
+        Arrays.fill(count, 0);
+ 
+        // Store count of occurrences in count[]
+        for (i = 0; i < n; i++)
+            count[(int) ((sequencia[i] / exp) % 10)]++;
+ 
+        // Change count[i] so that count[i] now contains
+        // actual position of this digit in output[]
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+ 
+        // Build the output array
+        for (i = n - 1; i >= 0; i--) {
+            output[count[(int) ((sequencia[i] / exp) % 10)] - 1] = (int) sequencia[i];
+            count[(int) ((sequencia[i] / exp) % 10)]--;
+        }
+ 
+        // Copy the output array to arr[], so that arr[] now
+        // contains sorted numbers according to current
+        // digit
+        for (i = 0; i < n; i++)
+            sequencia[i] = output[i];
+    }
+ 
+    // The main function to that sorts arr[] of
+    // size n using Radix Sort
+    public static void radixsort(long[] sequencia, int n)
+    {
+        // Find the maximum number to know number of digits
+        int m = getMax(sequencia, n);
+ 
+        // Do counting sort for every digit. Note that
+        // instead of passing digit number, exp is passed.
+        // exp is 10^i where i is current digit number
+        for (int exp = 1; m / exp > 0; exp *= 10)
+            countSort(sequencia, n, exp);
+    }
 }
